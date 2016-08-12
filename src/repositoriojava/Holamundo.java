df769f2b810478884a4f96620137f8ffa9e70fb5
matias <matiasallegranza06@yahoo.com.ar>
2016-08-12 19:26:42 -0300

    algo

--- src/repositoriojava/Holamundo.java
+++ src/repositoriojava/Holamundo.java
@@ -3,7 +3,8 @@ package repositoriojava;
 public class Holamundo {
 	//hola
 	public static void main(String[] args) {
-		// Proyecto GIT java 05082016
+		// Proyecto GIT java 05082016;
+		int a = 0;
 		System.out.println("123456");
 	}
 
