0d8f10588e2507d2a1fd3ad248ac987c4cb7f53b
amaya-florencia@outlook.com <fma>
2016-08-12 20:13:15 -0300

    ghhhhhhhh

--- src/repositoriojava/Holamundo.java
+++ src/repositoriojava/Holamundo.java
@@ -5,7 +5,7 @@ public class Holamundo {
 	public static void main(String[] args) {
 		// Proyecto GIT java 05082016
 		String a="hola, no sabemos que tocamos";
-		System.out.println("123456");
+		System.out.println("123456");//bhghjfjfhgf
 		
 	}
 
